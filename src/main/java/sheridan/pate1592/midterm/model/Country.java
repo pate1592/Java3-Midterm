a5efa9b8ca6b75824e28655fb450cc58505f430a
Khushaliben Patel <pate1592@sheridancollege.ca>
2021-06-24 16:35:41 -0400

    Country Created

---
+++ src/main/java/sheridan/pate1592/midterm/model/Country.java
@@ -0,0 +1,93 @@
+package sheridan.pate1592.midterm.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Country implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "continent")
+	private String continent;
+	@Column(name = "region")
+	private String region;
+	@Column(name = "surface_area")
+	private float surfaceArea;
+	@Column(name = "population")
+	private int population;
+
+	public Country() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContinent() {
+		return continent;
+	}
+
+	public void setContinent(String continent) {
+		this.continent = continent;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public float getSurfaceArea() {
+		return surfaceArea;
+	}
+
+	public void setSurfaceArea(float surfaceArea) {
+		this.surfaceArea = surfaceArea;
+	}
+
+	public int getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+
+	@Override
+	public String toString() {
+		return "Country [id=" + id + ", name=" + name + ", continent=" + continent + ", region=" + region
+				+ ", surfaceArea=" + surfaceArea + ", population=" + population + "]";
+	}
+
+}

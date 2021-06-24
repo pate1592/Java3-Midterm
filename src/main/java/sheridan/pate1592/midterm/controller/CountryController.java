b036c8ce45d59d00eea0863f974d69a445fcd938
Khushaliben Patel <pate1592@sheridancollege.ca>
2021-06-24 17:14:27 -0400

    html files done

--- src/main/java/sheridan/pate1592/midterm/controller/CountryController.java
+++ src/main/java/sheridan/pate1592/midterm/controller/CountryController.java
@@ -5,7 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import sheridan.pate1592.midterm.model.Country;
 import sheridan.pate1592.midterm.service.CountryService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class CountryController {
@@ -16,17 +21,20 @@ public class CountryController {
 		this.countryService = countryService;
 	}
 
-	@GetMapping(value = { "/index" })
+	@GetMapping(value = {"/", "/index" })
 	public String index(Model model) {
 
 		model.addAttribute("countries", countryService.findAllCountry());
-		model.addAttribute("count", countryService.count());
+		model.addAttribute("count",countryService.count());
 		return "CountryList";
 	}
 
 	@GetMapping("/countryDetails/{id}")
 	public String String(@PathVariable int id, Model model) {
-		model.addAttribute("country", countryService.findById(id));
+		Optional<Country> country = countryService.findById(id);
+		List<Optional<Country>> countries = new ArrayList<>();
+		countries.add(country);
+		model.addAttribute("countries", countries);
 		return "CountryDetails";
 	}
 

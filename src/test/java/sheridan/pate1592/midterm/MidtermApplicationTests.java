caa3d80de2010e9fb5e1b7f3e75dfb9a57489933
Khushaliben Patel <pate1592@sheridancollege.ca>
2021-06-24 17:15:07 -0400

    project completed

---
+++ src/test/java/sheridan/pate1592/midterm/MidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.pate1592.midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
